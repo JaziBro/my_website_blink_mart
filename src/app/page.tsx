497faf649f3f7b8e340b89a641642840c5df4d5a
jazilhashmi <138767638+JaziBro@users.noreply.github.com>
2024-06-11 20:08:31 +0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import Image from 'next/image';
 import Sale from './components/Sale';
 import Categories from './components/Categories';
-import New_products from './components/New_Products';
-import { getProducts } from './Helpers/page';
+
 
 export default async function Home () {
-  const products = await getProducts()
-  console.log(products)
   return (
    <>
     <Sale/>
     <Categories/>
-    <New_products/>
    </>
   )
 }
